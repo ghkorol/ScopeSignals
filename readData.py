cc0589ddfabcda3da86eae7f18a217dde3dc8cdb
ghkorol <jeka89.23@gmail.com>
2016-10-14 13:46:38 +0200

    Create readData.py
    Initial creation of readData.py

---
+++ readData.py
@@ -0,0 +1,16 @@
+import gpib
+	import time
+	tds= gpib.find("tds744") # define in /etc/gpib.conf
+	 
+	def query(handle, command, numbytes=100000):
+		gpib.write(handle,command)
+		#time.sleep(0.1)
+		response = gpib.read(handle,numbytes)
+		print response
+	 
+	gpib.write(tds,"DATA:ENCDG ASCI") #read data in ASCI 
+	gpib.write(tds,"DATA:WIDTH 1") #read numbers in 1 byte (-128 .. 128 range)
+	query(tds,"*IDN?")
+
+	 
+	gpib.close(tds)
