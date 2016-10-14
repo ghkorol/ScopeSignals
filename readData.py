46b3745d72b7bf36d3534b0eebca39fded1be4a6
ghkorol <jeka89.23@gmail.com>
2016-10-14 16:08:36 +0200

    1. Cleaning  2.Add "go to the Factory settings"

--- readData.py
+++ readData.py
@@ -1,19 +1,18 @@
 import gpib
-	import time
-	tds= gpib.find("tds744") # define in /etc/gpib.conf
-	 
-	def query(handle, command, numbytes=100000):
-		gpib.write(handle,command)
-		#time.sleep(0.1)
-		response = gpib.read(handle,numbytes)
-		print response
-	#line from ievgen 
-        #line 2
-	#line 3
-	#line from max
-	gpib.write(tds,"DATA:ENCDG ASCI") #read data in ASCI 
-	gpib.write(tds,"DATA:WIDTH 1") #read numbers in 1 byte (-128 .. 128 range)
-	query(tds,"*IDN?")
+import time
+tds= gpib.find("tds744") # define in /etc/gpib.conf
 
-	 
-	gpib.close(tds)
+def query(handle, command, numbytes=100000):
+	gpib.write(handle,command)
+	#time.sleep(0.1)
+	response = gpib.read(handle,numbytes)
+	print response
+
+query(tds,"*IDN?") #ask for ID, simple test if the scope does response (page 2-21 of Programmer Manual)
+gpib.write(tds,"FAC") #Reset to factory default (p. 2-21)
+gpib.write(tds,"DATA:ENCDG ASCI") #set the convenient data reading format(p. 2-34, p.2-35)
+gpib.write(tds,"DATA:WIDTH 1") #1 byte for reading numbers (-128 .. 128 range)(p. 2-34, p.2-35)
+
+
+	
+gpib.close(tds)
