822a666f3799873c7fba299c145019c6752bc535
ghkorol <jeka89.23@gmail.com>
2016-10-19 00:12:55 +0200

    Reading data by trigger

--- readData.py
+++ readData.py
@@ -1,15 +1,19 @@
 
-import os
+nTrigs = 100 #number of triggers is going to be stored 
 
+import os
 i = 0
 while os.path.exists("run%s.txt" % i):
     i += 1
 
+#Create output file
 import sys
 f = open("run%s.txt" % i, "w")
-sys.stdout = f # redirect output for all prints to the file
+#sys.stdout = f # redirect output for all prints to the file
 
-
+#print data and time
+from datetime import datetime
+f.write(str(datetime.now())+"\n")
 
 import gpib
 import time
@@ -17,34 +21,70 @@ tds= gpib.find("tds744") # define in /etc/gpib.conf
 
 def query(handle, command, numbytes=100000):
 	gpib.write(handle,command)
-	#time.sleep(0.1)
+	time.sleep(0.01)
 	response = gpib.read(handle,numbytes)
-	print response
+	response = response.replace('\n','')
+	return response
+def write(handle, command):
+  gpib.write(handle,command)
+  time.sleep(2)
 
-query(tds,"*IDN?") #ask for ID, simple test if the scope does response (page 2-21 of Programmer Manual)
-gpib.write(tds,"DATA:ENCDG ASCI") #set the convenient data reading format(p. 2-34, p.2-35)
-gpib.write(tds,"DATA:WIDTH 1") #1 byte for reading numbers (-128 .. 128 range)(p. 2-34, p.2-35)
+print query(tds,"*IDN?") #ask for ID, simple test if the scope does response (page 2-21 of Programmer Manual)
 
 #Set Parameters
-gpib.write(tds,"FAC") #Reset to factory default (p. 2-21)
-time.sleep(5)
-gpib.write(tds,"BELL")
-gpib.write(tds,"SELECT:CH1 ON;CH2 ON;CH3 OFF;CH4 OFF") #Ch on or off
-gpib.write(tds,"HOR:MAI:SCAl 100.0E-9") #Set Time Scale
-gpib.write(tds,"CH1:SCAL 500.0E-3 ") # Set Voltage Scale
-gpib.write(tds,"CH1:IMP FIF ") # SET Impedance to 50 Ohm, for 1M use MEG
-gpib.write(tds,"CH1:POS 0") #y-Position
-gpib.write(tds,"CH2:SCAL 500.0E-3 ") 
-gpib.write(tds,"CH2:IMP FIF ") 
-gpib.write(tds,"CH2:POS 0")
+write(tds,"FAC") #Reset to factory default (p. 2-21)
+time.sleep(10)
+write(tds,"BELL")
+write(tds,"SELECT:CH1 ON;CH2 ON;CH3 ON;CH4 OFF") #Ch on or off
+write(tds,"DATA:SOURCE  CH1,CH2,CH3")#Set channel for feding data from
+write(tds,"HOR:MAI:SCAl 100.0E-9") #Set Time Scale
+write(tds,"CH1:SCAL 200.0E-3 ") # Set Voltage Scale
+write(tds,"CH1:IMP FIF ") # SET Impedance to 50 Ohm, for 1M use MEG
+write(tds,"CH1:POS 0") #y-Position
+write(tds,"CH2:SCAL 200.0E-3 ") 
+write(tds,"CH2:IMP FIF ") 
+write(tds,"CH2:POS 0")
+write(tds,"CH3:SCAL 200.0E-3 ") 
+write(tds,"CH3:IMP FIF ") 
+write(tds,"CH3:POS 0")
 
 #Set Trigger
-gpib.write(tds,"TRIG:MAI:EDGE:SLO FALL") #change Slope Fall or Rise
-gpib.write(tds,"TRIG:MAI:EDGE:SOU CH1") #Trigger Source
-gpib.write(tds,"TRIG:MAI:LEV -100.0E-3") #Trigger y-Position
-	
+write(tds,"TRIG:MAI:EDGE:SLO FALL") #change Slope Fall or Rise
+write(tds,"TRIG:MAI:EDGE:SOU CH1") #Trigger Source
+write(tds,"TRIG:MAI:LEV -200.0E-3") #Trigger y-Position
+#Set Data Format
+write(tds,"DATA:ENCDG ASCI") #set the convenient data reading format(p. 2-34, p.2-35)
+write(tds,"BELL")
+
+## Read data by trigger 
+trigsSaved = 0
+while trigsSaved<nTrigs:
+  breaker = False
+  AutoAuto = False
+  while 0<1:
+    trigState1 = query(tds,"TRIG:STATE?")
+    trigState2 = query(tds,"TRIG:STATE?")
+    #print trigState1, " ", trigState2
+    if (trigState1 == ":TRIG:STATE AUTO" and trigState2 == ":TRIG:STATE AUTO"): AutoAuto = True
+    if (trigState1 == ":TRIG:STATE AUTO" and trigState2 != ":TRIG:STATE AUTO") or (AutoAuto and trigState1 != ":TRIG:STATE AUTO" and trigState2 != ":TRIG:STATE AUTO"):
+      AutoAuto = False
+      breakCounter = 0
+      while breakCounter<100:
+	trigState3 = query(tds,"TRIG:STATE?")
+	if trigState3 == ":TRIG:STATE REA":
+	  gpib.write(tds,"BELL")
+	  f.write(query(tds,"CURVE?")+"\n")
+	  breaker = True
+	  break
+	breakCounter += 1
+      if breaker:
+	trigsSaved += 1
+	f.write("saved trigers: %s\n" % trigsSaved) 
+	time.sleep(0.5)
+	break
+
+
+
+
 gpib.close(tds)
-
-
-
 f.close()
